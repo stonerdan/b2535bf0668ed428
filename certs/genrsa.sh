bc2c5541c24fe6a968cd055f6f962d00e9fe4893
Dan Stoner <stonerdan@mailinator.com>
2011-06-08 08:49:47 -0500

    User SHA256 on RSA signatures

--- certs/genrsa.sh
+++ certs/genrsa.sh
@@ -2,5 +2,5 @@
 
 openssl genrsa -out server.rsakey 1024
 openssl req -new -key server.rsakey -out server.rsacsr -subj '/CN=localhost'
-openssl x509 -req -in server.rsacsr -signkey server.rsakey -out server.rsacrt -days 30
+openssl x509 -req -in server.rsacsr -signkey server.rsakey -out server.rsacrt -days 30 -sha256
 
