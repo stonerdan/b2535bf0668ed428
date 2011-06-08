5f9a803f87743f65b6d1958387914d266f590be3
Dan Stoner <stonerdan@mailinator.com>
2011-06-08 08:54:01 -0500

    Update key generation dirs

--- genrsa.sh
+++ genrsa.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-openssl genrsa -out server.rsakey 1024
-openssl req -new -key server.rsakey -out server.rsacsr -subj '/CN=localhost'
-openssl x509 -req -in server.rsacsr -signkey server.rsakey -out server.rsacrt -days 30 -sha256
+mkdir -p certs
+
+openssl genrsa -out certs/server.rsakey 1024
+openssl req -new -key certs/server.rsakey -out certs/server.rsacsr -subj '/CN=localhost'
+openssl x509 -req -in certs/server.rsacsr -signkey certs/server.rsakey -out certs/server.rsacrt -days 30 -sha256
 
