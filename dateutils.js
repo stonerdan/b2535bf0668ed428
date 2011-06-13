ec5d28fcdb0b44d36b1d8b56aad1ba7140d73668
Dan Stoner <stonerdan@mailinator.com>
2011-06-13 01:56:34 -0500

    Moved date functions into separate file

---
+++ dateutils.js
@@ -0,0 +1,18 @@
+
+Date.prototype.toISODateString = function(d) {
+    function pad(n){
+        return n < 10 ? '0' + n : n
+    }
+    function pad3(n){
+        if (n < 10) return '00' + n;
+        if (n < 100) return '0' + n;
+        return n;
+    }
+    return this.getUTCFullYear()+'-'
+    + pad(this.getUTCMonth()+1)+'-'
+    + pad(this.getUTCDate())+'T'
+    + pad(this.getUTCHours())+':'
+    + pad(this.getUTCMinutes())+':'
+    + pad(this.getUTCSeconds())+'.'
+    + pad3(this.getUTCMilliseconds())+'Z'
+}
--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 var server = require('http');
 var fs     = require('fs');
 var port   = 8000;
+require('./dateutils.js');
 
 if (require('path').existsSync('logs') == false) {
     fs.mkdirSync('logs', 0755);
@@ -14,24 +15,6 @@ var len = name.length;
 var uaStrings = {};
 var test = 0;
 
-function ISODateString(d) {
-    function pad(n){
-        return n < 10 ? '0' + n : n
-    }
-    function pad3(n){
-        if (n < 10) return '00' + n;
-        if (n < 100) return '0' + n;
-        return n;
-    }
-    return d.getUTCFullYear()+'-'
-    + pad(d.getUTCMonth()+1)+'-'
-    + pad(d.getUTCDate())+'T'
-    + pad(d.getUTCHours())+':'
-    + pad(d.getUTCMinutes())+':'
-    + pad(d.getUTCSeconds())+'.'
-    + pad3(d.getUTCMilliseconds())+'Z'
-}
-
 var tlsOptions = {
 	key: fs.readFileSync('certs/server.rsakey'),
 	cert: fs.readFileSync('certs/server.rsacrt')
@@ -44,7 +27,7 @@ function HandleRequest(req, res) {
     if (!uaStrings[userAgent])
         uaStrings[userAgent] = 'tits' + test++;
 
-    uaLog.write(ISODateString(d) + ': ' + userAgent + '\n');
+    uaLog.write(d.toISODateString() + ': ' + userAgent + '\n');
     res.writeHead(200, {'Content-type': 'text/plain;charset=utf-8'});
     res.end('Date: ' + d.getTime());
 }
