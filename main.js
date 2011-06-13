98418d6a7e87fe368e78398ee87799b5181a0095
Dan Stoner <stonerdan@mailinator.com>
2011-06-13 01:57:20 -0500

    Remove unused code

--- main.js
+++ main.js
@@ -10,11 +10,6 @@ if (require('path').existsSync('logs') == false) {
 var uaLog = fs.createWriteStream('logs/user-agents.log',
                                  {flags: 'a', encoding: 'utf8'});
 
-var name = 'Dejan Čančarević';
-var len = name.length;
-var uaStrings = {};
-var test = 0;
-
 var tlsOptions = {
 	key: fs.readFileSync('certs/server.rsakey'),
 	cert: fs.readFileSync('certs/server.rsacrt')
@@ -23,9 +18,6 @@ var tlsOptions = {
 function HandleRequest(req, res) {
     var d = new Date();
     var userAgent = req.headers['user-agent'];
-    
-    if (!uaStrings[userAgent])
-        uaStrings[userAgent] = 'tits' + test++;
 
     uaLog.write(d.toISODateString() + ': ' + userAgent + '\n');
     res.writeHead(200, {'Content-type': 'text/plain;charset=utf-8'});
